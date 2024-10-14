fd358357dc96afb40dba41dbcbd0eb1c88c24718
atlas-itrc <itrcplay@gmail.com>
2024-10-14 15:10:18 +0500

    completed readability

--- 2/readability.c
+++ 2/readability.c
@@ -1,25 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-int main(int argc, char **argv) {
+float indexCalculator(char string[], int size);
+
+int main(int argc, char **argv)
+{
     char *input = NULL;
     size_t size = 0;
     int ch;
-    
+
     printf("Enter a sentence: ");
-    
-    while ((ch = getchar()) != '\n' && ch != EOF) {
-        input = realloc(input, size + 2);  // Dynamically increase size
-        if (!input) {
+
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        input = realloc(input, size + 2); // Dynamically increase size
+        if (!input)
+        {
             perror("Memory allocation failed");
             return 1;
         }
         input[size++] = ch;
     }
-    input[size] = '\0';  // Null-terminate the string
+    input[size] = '\0'; // Null-terminate the string
 
-    printf("\nThe sentence you entered is: %s\n", input);
+    float x = indexCalculator(input, size);
+    printf("Index is: %f\n", x);
 
-    free(input);  // Free the dynamically allocated memory
+    
+
+    free(input); // Free the dynamically allocated memory
     return 0;
+}
+
+//Coleman Liau Index formula: 5.89 x (characters/words) – 0.3 x (sentences/words) – 15.8
+float indexCalculator(char string[], int size)
+{
+    float no_of_characters = 0;
+    float no_of_words = 1;
+    float no_of_sentences = 0;
+    for(int i = 0; i < size; i++) {
+        string[i] = tolower(string[i]);
+        if(string[i] == ' ') {
+            no_of_words++;
+        }
+        if(string[i] == '.' || string[i] == '!' || string[i] == '?') {
+            no_of_sentences++;
+        }
+        if(string[i] >= 'a' && string[i] <= 'z') {
+            no_of_characters++;
+        }
+    }
+    printf("Total number of characters are: %f\n", no_of_characters);
+    printf("Total number of words are: %f\n", no_of_words);
+    printf("Total number of sentences are: %f\n", no_of_sentences);
+    float index = 0.0588 * (100 * no_of_characters / no_of_words) - 0.296 * (100 * no_of_sentences / no_of_words) - 15.8;
+    return index;
 }
